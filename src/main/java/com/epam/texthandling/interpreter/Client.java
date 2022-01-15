996eace3f7c1f6fb6d749dba846f9035a9f65c21
partizan1317 <papa.b.gorode@gmail.com>
2022-01-15 21:31:22 +0300

    Added implementation of class client and new class TerminalExpression

--- src/main/java/com/epam/texthandling/interpreter/Client.java
+++ src/main/java/com/epam/texthandling/interpreter/Client.java
@@ -1,5 +1,53 @@
 package com.epam.texthandling.interpreter;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Client {
+    
+    private final ArrayList<AbstractMathExpression> listExpression;
+
+    public Client(String expression) {
+        listExpression = new ArrayList<>();
+
+    }
+
+    private void parse(String expression) {
+        for (String lexeme : expression.split("\\p{Blank}+")) {
+            if (lexeme.isEmpty()) {
+                continue;
+            }
+            char temp = lexeme.charAt(0);
+            switch(temp) {
+                case '+':
+                    listExpression.add(new TerminalExpressionPlus());
+                    break;
+                case '-':
+                    listExpression.add(new TerminalExpressionMinus());
+                    break;
+                case '/':
+                    listExpression.add(new TerminalExpressionDivide());
+                    break;
+                case '*':
+                    listExpression.add(new TerminalExpressionMultiply());
+                    break;
+                default:
+                    Scanner scan = new Scanner(lexeme);
+                    if(scan.hasNextInt()) {
+                        listExpression.add(new NonterminalExpressionNumber(scan.nextInt()));
+                    }
+
+            }
+
+        }
+    }
+
+    public Number calculate() {
+        Context context = new Context();
+        for (AbstractMathExpression terminal : listExpression) {
+            terminal.interpret(context);
+        }
+        return context.popValue();
+    }
 
 }
---
+++ src/main/java/com/epam/texthandling/interpreter/TerminalExpression.java
@@ -0,0 +1,4 @@
+package com.epam.texthandling.interpreter;
+
+public class TerminalExpression {
+}
